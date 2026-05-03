3cb53c2d6c484b41653d21722f61e0a399ec8b89
AlexWambui <alexwambuik@gmail.com>
2026-05-03 18:42:37 +0300

    modified the app layout to use app header and added a component for showing the app name.

---
+++ resources/js/components/custom/AppName.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+const name = page.props.name;
+</script>
+
+<template>
+    <slot>
+        {{ name }}
+    </slot>
+</template>
